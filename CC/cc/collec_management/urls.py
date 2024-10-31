a8efaa9e5ce2d0eba905d83d17c6d777d499789b
BelbazMelina <Belbazmelina@gmail.com>
2024-10-31 23:09:15 +0100

    Q5,6,7

--- CC/cc/collec_management/urls.py
+++ CC/cc/collec_management/urls.py
@@ -1,10 +1,25 @@
-<<<<<<< HEAD
+
 from django.contrib import admin
 from django.urls import path
 from . import views 
 
 urlpatterns = [
-       path('collection/1/', views.collection_detail, name='collection_detail'),
        path ("about/",views.about, name="about"),
+]
+#Q5
+urlpatterns = [
+       path('collection/<int:n>/', views.collection_detail, name='collection_detail'),
+   ]
+
+#Q6
+urlpatterns = [
+  path('collection/<int:n>/', views.collection_detail, name='collection_detail'),
+  path('all/', views.collection_list, name='collection_list'),  # Nouvelle route pour la liste des collections
+]
+#Q8
+urlpatterns = [
+  path('collection/<int:n>/', views.collection_detail, name='collection_detail'),
+  path('all/', views.collection_list, name='collection_list'),
+  path('delete/<int:n>/', views.collection_delete, name='collection_delete'),  # Route pour la suppression
 ]
    
--- CC/cc/collec_management/views.py
+++ CC/cc/collec_management/views.py
@@ -1,18 +1,35 @@
-from django.shortcuts import render
-<<<<<<< HEAD
+from django.shortcuts import render,redirect
 from django.shortcuts import render, get_object_or_404
 from .models import Collec
 
 # Create your views here.
  
 
+       
+def about(request):
+    return render(request,'collec_management/presentation.html')
+
+    #QUESTION 5
 def collection_detail(request, n):
        collection = get_object_or_404(Collec, id=n)
        return render(request
        ,'collec_management/collection_detail.html'
        , {'collection': collection})
-       
-def about(request):
-    return render(request,'collec_management/presentation.html')
 
-    
+#Q6
+
+def collection_list(request):
+  collections = Collec.objects.all()
+  context = {'collections': collections}
+  return render(request, 'collec_management/collection_list.html', context)
+
+#Q8
+def collection_delete(request, n):
+  collection = get_object_or_404(Collec, id=n)
+  
+  if request.method == 'POST':
+      collection.delete()
+      return redirect('collection_list') 
+
+ 
+  return render(request, 'collec_management/collection_confirm_delete.html', {'collection': collection})
