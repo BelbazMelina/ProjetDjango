61e31561411931deca72b8052211f519477bfbdc
HIBAMAZER <hibat-allah.mazer@etu.univ-orleans.fr>
2024-10-31 21:55:54 +0100

    creation model collect

--- CC/cc/collec_management/admin.py
+++ CC/cc/collec_management/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Collec
+admin.site.register(Collec)
--- CC/cc/collec_management/models.py
+++ CC/cc/collec_management/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Collec(models.Model):
+    title = models.CharField(max_length=100, null=False,blank=False)
+    description = models.TextField(null=False,blank=False)
+    date_creation = models.DateField(null=False, blank=False)
+
+    def __str__(self):
+        return self.title
+    
+    
